07234de79fb89d79564c99b5c1f98f7d2f919b92
Marcilio <marcilio.pa@hotmail.com>
2018-04-06 16:16:21 -0300

    Elementos organizados

--- PDF_c.php
+++ PDF_c.php
@@ -3,12 +3,11 @@
     require_once("conexao/conexao.php"); //conexão com o banco
 
 
-class PDF_c extends mpdf
-{
+    class PDF_c extends mpdf {
 
     // Atributos da classe
     private $pdo = null;
-    private $mpdf = null;
+    private $pdf = null;
     private $css = null;
     private $titulo = null;
 
@@ -19,6 +18,7 @@ class PDF_c extends mpdf
     */
     public function __construct($css, $titulo)
     {
+
         $this->pdo = Conexao::getInstance();
         $this->titulo = $titulo;
         $this->setarCSS($css);
@@ -37,100 +37,79 @@ class PDF_c extends mpdf
         endif;
     }
 
-    //Estrutura antiga
-    /*
-     // Construção do PDF em HTML
-     $html = "";
-     $html .= "<fieldset>";
-     $html .= "<img src=\"img\logo1.jpg\">";
-     $html .= "<div class='header'>";
-     $html .= "<h2>FUNDAÇÃO DE SAÚDE DE VITÓRIA DA CONQUISTA $data</h2>";
-     $html .= "</div>";
-     $html .= "<div class='header'>";
-     $html .= "<h2>MATERIAL DE ESCRITÓRIO</h2>";
-     $html .= "</div>";
-     $html .= "<table border='1' width='1000' align='center'>";
-     $html .= "<tr class='header'>";
-     $html .= "<th>Discriminação Detalhada do Produto</th>";
-     $html .= "<th>Estoque do Almoxarifado</th>";
-     $html .= "<th>Estoque Total</th>";
-     $html .= "<th>Valor Unitário</th>";
-     $html .= "<th>Valor Total</th>";
-     $html .= "</tr>";
+    //Construção do HTML para o PDF
+    public function CorpoHTML() {
 
-     $sql = "select * from produtos";
-     foreach ($pdo->query($sql) as $reg):
-        $html .= ($color) ? "<tr>" : "<tr class=\"zebra\">";
-        $html .= "<td class='destaque'>{$reg['disc_produto']}</td>";
-        $html .= "<td>{$reg['qt_total']}</td>"; //quantidade total do estoque
-        $html .= "<td>{$reg['qt_atual']}</td>"; //quantidade atual do estoque
-        $html .= "<td>{$reg['vl_unitario']}</td>"; //valor unitário do produto
-        $html .= "<td>{$reg['vl_total']}</td>"; //valor total do produto
-        $html = !$color;
-     endforeach;
-
-     $html .= "</table>";
-     return $html;
-     $html .= "</table>";
-     $html .= "<div class='principal'>&nbsp;</div>";
-     $html .= "</fieldset>";
-    */
-
-    //Construção do HTML
-    public function CorpoHTML()     {
-
+        $data = date('d/m/Y');
         $color = null;
         $html .= "
-    <fieldset>
+        <fieldset>
         <img src=\"img\logo1.jpg\">
         <div class='header'>
-            <h2>FUNDAÇÃO DE SAÚDE DE VITÓRIA DA CONQUISTA $data</h2>
+            <h2>FUNDACAO DE SAUDE DE VITORIA DA CONQUISTA $data</h2>
         </div>
         <div class='header'>
-            <h2>MATERIAL DE ESCRITÓRIO</h2>
-        </div>
-        <table border='1' width='1000' align='center'>
+            <h2>MATERIAL DE ESCRITORIO</h2>
+        </div>";
+        $html .= " <table border='1' width='1000' align='center'>
         <tr class='header'>
-            <th>Discriminação Detalhada do Produto</th>
-            <th>Estoque do Almoxarifado</th>
-            <th>Estoque Total</th>
-            <th>Valor Unitário</th>
-            <th>Valor Total</th>
+            <th class='center'>ITEM</th>
+            <th class='center'>DISCRIMINACAO DETALHADA DO PRODUTO</th>
+            <th id='quebra' class='center'>ESTOQUE DO ALMOXARIFADO</th>
+            <th id='quebra' class='center'>ESTOQUE ATUAL</th>
+            <th class='center'>VALOR UNITARIO</th>
+            <th class='center'>VALOR TOTAL</th> 
         </tr>";
 
         //Chamada do SQL
+        $count = 1;
         $sql = "select * from produtos";
         foreach ($this->pdo->query($sql) as $reg):
             $html .= ($color) ? "<tr>" : "<tr class=\"zebra\">";
-            $html .= "<td>{$reg['disc_produto']}</td>";
-            $html .= "<td>{$reg['qt_total']}</td>"; //quantidade total do estoque
-            $html .= "<td>{$reg['qt_atual']}</td>"; //quantidade atual do estoque
-            $html .= "<td>{$reg['vl_unitario']}</td>"; //valor unitário do produto
-            $html .= "<td>{$reg['vl_total']}</td>"; //valor total do produto
+            $html .= "<td class='center'>$count";
+            $html .= "</td>";
+            $html .= "<td class='left'>{$reg['disc_produto']}</td>"; //descrição do produto
+            $html .= "<td class='center'>{$reg['qt_total']}</td>"; //quantidade total do estoque
+            $html .= "<td class='center'>{$reg['qt_atual']}</td>"; //quantidade atual do estoque
+            $html .= "<td class='left'>R$ {$reg['vl_unitario']}</td>"; //valor unitário do produto
+            $html .= "<td class='left'>R$ {$reg['vl_total']}</td>"; //valor total do produto
             $color = !$color;
         endforeach;
-        $html .= "</table>";
-        $html .= "<div class='principal'>&nbsp;</div>";
-        $html .= "</fieldset>";
+
+        $html .= "
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        <td class='left'>TOTAL</td>";
+        //Soma do total do vl_total de cada item da lista
+        $soma = "select sum(vl_total) from produtos";
+        foreach ($this->pdo->query($soma) as $resultado);
+        $html .= "<td class='left'>R$ {$resultado[0]}</td>
+        </tr>
+        </table>
+        </fieldset>";
 
         return $html;
     }
 
     public function GerarPDF_M_E() {
     // Geração do PDF
-    $this->mpdf = new mPDF('utf-8', 'A4');
+    $this->pdf = new mPDF('utf-8', 'A4');
     //Trata caracteres especiais sem gerar erro
-    $this->mpdf->allow_charset_conversion = true;
-    //$this->mpdf->charset_in='iso-8859-1';
-    $this->mpdf->charset_in = 'windows-1252';
-    $this->mpdf->SetDisplayMode('fullpage');
+    $this->pdf->allow_charset_conversion = true;
+    $this->pdf->charset_in='iso-8859-1';
+    //$this->pdf->charset_in = 'windows-1252';
+    $this->pdf->SetDisplayMode('fullpage');
     $css = file_get_contents("css/estilo.css");
-    $this->mpdf->WriteHTML($css, 1);
-    $this->mpdf->WriteHTML($this->CorpoHTML());
-    $this->mpdf->Output();
+    $this->pdf->WriteHTML($css, 1);
+    $this->pdf->WriteHTML($this->CorpoHTML());
+    $this->pdf->Output();
+    $this->pdf = new mPDF(['tempDir' => __DIR__ . '/tmp']);
+    ob_clean(); //limpa o objeto dos dados do pdf
     exit;
     }
-
 /*
     * Método para exibir o arquivo PDF
     * @param $name - Nome do arquivo se necessário grava-lo
